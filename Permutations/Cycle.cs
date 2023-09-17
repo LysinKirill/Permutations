e886db194a4c84c8c73c9670ac5126c93751c848
Lysin Kirill <51096131+LysinKirill@users.noreply.github.com>
2023-09-18 01:28:53 +0300

    Permutations v1.0.0

---
+++ Permutations/Cycle.cs
@@ -0,0 +1,121 @@
+﻿namespace Permutations;
+using System;
+public class Cycle
+{
+    private int[] arr;
+    private int maxEl;
+
+    public int GetEl(int ind) => arr[ind];
+    public Cycle(params int[] a)
+    {
+        if (!Check(a))
+            throw new Exception("Такой цикл создать невозможно");
+        arr = a;
+        maxEl = -1;
+        foreach (int x in a)
+            if (x > maxEl)
+                maxEl = x;
+    }
+
+    public int getMax() => maxEl;
+
+    static bool Check(int[] a)
+    {
+        int max = a.Max();
+        bool[] b = new bool[max];
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (b[a[i] - 1])
+                return false;
+            b[a[i] - 1] = true;
+        }
+
+        return true;
+    }
+    
+    public Permutation ToPermutation()
+    {
+        return ToPermutation(this);
+    }
+
+    public static bool TryParse(string s, out Cycle c)
+    {
+        if (s.Length == 0)
+        {
+            c = new Cycle(new int[]{});
+            return false;
+        }
+
+        try
+        {
+            c = Parse(s);
+            return true;
+        }
+        catch (Exception e)
+        {
+            c = new Cycle(new int[]{});
+            return false;
+        }
+    }
+
+    public static Cycle Parse(string s)
+    {
+        // переделать чтобы было со скобками
+        string[] k = s.Split(" ");
+        int[] a = new int[k.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = int.Parse(k[i]);
+        }
+
+        return new Cycle(a);
+    }
+
+    public int GetOrder()
+    {
+        return ToPermutation(this).GetOrder();
+    }
+
+    public static Permutation ToPermutation(Cycle c, int len)
+    {
+        if (len < c.maxEl)
+            throw new Exception();
+        
+        int[] a = new int[len];
+        for (int i = 0; i < c.arr.Length; i++)
+        {
+            if (i == c.arr.Length - 1)
+            {
+                a[c.arr[i] - 1] = c.arr[0];
+                break;
+            }
+
+            a[c.arr[i] - 1] = c.arr[i + 1];
+        }
+
+        for (int i = 0; i < len; i++)
+        {
+            if (a[i] != 0)
+                continue;
+            a[i] = i + 1;
+        }
+
+        return new Permutation(a);
+    }
+
+
+    public static Permutation ToPermutation(Cycle c)
+    {
+        return ToPermutation(c, c.maxEl);
+    }
+
+    public static Permutation operator *(Cycle a, Cycle b)
+    {
+        return ToPermutation(a, Math.Max(a.maxEl, b.maxEl)) * ToPermutation(b, Math.Max(a.maxEl, b.maxEl));
+    }
+
+    public override string ToString()
+    {
+        return "(" + String.Join(" ", arr) + ")";
+    }
+}
---
+++ Permutations/Program.cs
@@ -0,0 +1,96 @@
+﻿using Permutations;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, int> s3 = new Dictionary<int, int>();
+        foreach (var s in Permutation.S(3))
+        {
+            if (!s3.ContainsKey(s.GetOrder()))
+                s3[s.GetOrder()] = 0;
+            s3[s.GetOrder()]++;
+        }
+
+
+
+
+        Dictionary<int, int> s4 = new Dictionary<int, int>();
+        foreach (var s in Permutation.S(4))
+        {
+            if (!s4.ContainsKey(s.GetOrder()))
+                s4[s.GetOrder()] = 0;
+            s4[s.GetOrder()]++;
+        }
+
+        foreach (var pv in s3)
+            Console.WriteLine($"{pv.Key}: {pv.Value}");
+
+        Console.WriteLine("______________________");
+        foreach (var pv in s4)
+            Console.WriteLine($"{pv.Key}: {pv.Value}");
+
+    }
+    //Amogus a = new Amogus(new Permutation(2, 1, 3), new Permutation(3, 1, 2, 4), 3, 4);
+        //Console.WriteLine(a.GetOrder());
+
+        /*
+        List<Amogus> list = new List<Amogus>();
+        foreach (var s1 in Permutation.S(3))
+        {
+            foreach (var s2 in Permutation.S(4))
+            {
+                list.Add(new Amogus(s1, s2, 0, 4));
+                list.Add(new Amogus(s1, s2, 1, 4));
+                list.Add(new Amogus(s1, s2, 2, 4));
+                list.Add(new Amogus(s1, s2, 3, 4));
+                list.Add(new Amogus(s1, s2, 4, 4));
+            }
+        }
+
+        int counter = 0;
+        foreach(var x in list)
+            if (x.GetOrder() == 6)
+                counter++;
+        Console.WriteLine(counter);
+    }
+    */
+}
+
+class Amogus
+{
+    private Permutation p1, p2;
+    private int a;
+    private int m;
+
+    public Amogus(Permutation p1, Permutation p2, int a, int m)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.a = a;
+        this.m = m;
+    }
+
+    public int GetOrder()
+    {
+        int counter = 1;
+        Permutation v1 = p1.Copy();
+        Permutation v2 = p2.Copy();
+        int temp = a;
+        while (v1 != Permutation.Id(v1.GetLength()) || v2 != Permutation.Id(v2.GetLength()) || temp != 0)
+        {
+            // Console.WriteLine($"{v1}: {v1 == Permutation.Id(3)}");
+            // Console.WriteLine($"{v2}: {v2 == Permutation.Id(4)}");
+            // Console.WriteLine(temp);
+            // Console.WriteLine("_____________");
+            v1 = v1 * p1;
+            v2 = v2 * p2;
+            temp += a;
+            temp = temp % m;
+            counter++;
+        }
+
+        return counter;
+    }
+}
